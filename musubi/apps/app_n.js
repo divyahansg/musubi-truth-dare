509282ae9b8390281f5824c713dc28d293045874
Divyahans Gupta <divyahansg@gmail.com>
2012-07-10 14:11:12 -0700

    [object Object].html()

--- musubi/apps/app_n.js
+++ musubi/apps/app_n.js
@@ -40,7 +40,7 @@ Musubi.ready(function(context) {
       //	start_obj_DbObj = musu.appContext.feed.query("type='note'")[0];
       //}
       console.log("finished!");
-      console.log("start obj is: " + start_obj_DbObj.toString());
+      console.log("start obj is: " + start_obj_DbObj.html());
       //musu.appContext.start_obj_DbObj.post(truth_obj);
       
       //musu.appContext.feed.post(truth_obj);
