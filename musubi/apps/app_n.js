f67e24d2d2dee39c69a160975be4f55b852979d9
Nikhil <nikhil.narayen@gmail.com>
2012-07-10 11:49:48 -0700

    trying nesting

--- musubi/apps/app_n.js
+++ musubi/apps/app_n.js
@@ -34,8 +34,10 @@ Musubi.ready(function(context) {
       var dare_content = { "__html" : html, "text" : dare_text };
       var dare_obj = new SocialKit.Obj({type : "dare", json: dare_content});
       
-      var start_obj_DbObj = musu.appContext.feed.query("type='note'");
+      var start_obj_DbObj = musu.appContext.feed.query("type='note'")[0];
       console.log("blahblahblahblah" + start_obj_DbObj);
+      start_obj_DbObj.post(truth_obj);
+      
       //musu.appContext.feed.post(truth_obj);
       //musu.appContext.feed.post(dare_obj);
 
