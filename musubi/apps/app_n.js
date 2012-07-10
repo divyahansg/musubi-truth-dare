e0a9da9b1d6383e2e58a3980473b13e4364b380f
Nikhil <nikhil.narayen@gmail.com>
2012-07-10 14:02:17 -0700

    double array

--- musubi/apps/app_n.js
+++ musubi/apps/app_n.js
@@ -34,12 +34,13 @@ Musubi.ready(function(context) {
       var dare_content = { "__html" : html, "text" : dare_text };
       var dare_obj = new SocialKit.Obj({type : "dare", json: dare_content});
       
-      var start_obj_DbObj = musu.appContext.feed.query("type='note'")[0];
-      while (!(start_obj_DbObj instanceof SocialKit.DbObj))
-      {
-      	start_obj_DbObj = musu.appContext.feed.query("type='note'")[0];
-      }
+      var start_obj_DbObj = musu.appContext.feed.query("type='note'")[0][0];
+      //while (!(start_obj_DbObj instanceof SocialKit.DbObj))
+      //{
+      //	start_obj_DbObj = musu.appContext.feed.query("type='note'")[0][0];
+      //}
       console.log("finished!");
+      console.log("start obj is: " + start_obj_DbObj.toString());
       //musu.appContext.start_obj_DbObj.post(truth_obj);
       
       //musu.appContext.feed.post(truth_obj);
