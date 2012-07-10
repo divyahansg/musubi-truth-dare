560286d9d394d63bdb3e1e1299897ad55eacdba2
Nikhil <nikhil.narayen@gmail.com>
2012-07-10 11:01:42 -0700

    testing DbObj

--- musubi/apps/app_n.js
+++ musubi/apps/app_n.js
@@ -16,7 +16,7 @@ Musubi.ready(function(context) {
       var html = '<span style="' + style + '">' + text + '</span>';
       var content = { "__html" : html, "text" : text };
       start_obj = new SocialKit.Obj({type : "note", json: content}) //global
-      //musu.appContext.feed.post(start_obj);
+      musu.appContext.feed.post(start_obj);
       //musu.appContext.quit();
       console.log("start_obj looks like this after post:"+ start_obj );
       
@@ -34,13 +34,9 @@ Musubi.ready(function(context) {
       var dare_content = { "__html" : html, "text" : dare_text };
       var dare_obj = new SocialKit.Obj({type : "dare", json: dare_content});
       
-      console.log("before appcontext.feed");
-      
-      musu.appContext.feed.post(truth_obj);
-      musu.appContext.feed.post(dare_obj);
-      
-      console.log("after appcontext.feed/before start_obj.post");
-      console.log("start_obj looks like this:"+ start_obj );
+      //musu.appContext.feed.post(truth_obj);
+      //musu.appContext.feed.post(dare_obj);
+
       //start_obj.post(truth_obj);
       //start_obj.post(dare_obj);
       
