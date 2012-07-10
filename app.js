99596f6827e8123ce7ac5aa2efce1275b5606b9d
Divyahans Gupta <divyahansg@gmail.com>
2012-07-09 17:42:12 -0700

    added app.js

---
+++ app.js
@@ -0,0 +1,75 @@
+function MusuWriter(app) {
+  this.appContext = app;
+}
+
+var musu;
+var start_obj; //global
+Musubi.ready(function(context) {
+    console.log("launching bigwords.");
+    musu = new MusuWriter(context);
+    
+    $("#start").click(function(e) {
+      var style = "font-size:30px;padding:5px;";
+      style += "background-color:blue;white-space:nowrap;";
+      style += "color:red;";
+      var text = "game started!";
+      var html = '<span style="' + style + '">' + text + '</span>';
+      var content = { "__html" : html, "text" : text };
+      start_obj = new SocialKit.Obj({type : "note", json: content}) //global
+      musu.appContext.feed.post(start_obj);
+      musu.appContext.quit();
+      console.log("start_obj looks like this after post:"+ start_obj );
+      
+    });
+    
+    
+    $("#submit").click(function(e) {
+    /**
+      var truth_text = $("#truth").val();
+      var html = '<span>' + truth_text + '</span>';
+      var truth_content = { "__html" : html, "text" : truth_text };
+      var truth_obj = new SocialKit.Obj({type : "truth", json: truth_content});
+      
+      var dare_text = $("#dare").val();
+      html = '<span>' + dare_text + '</span>';
+      var dare_content = { "__html" : html, "text" : dare_text };
+      var dare_obj = new SocialKit.Obj({type : "dare", json: dare_content});
+      
+      console.log("before appcontext.feed");
+      
+      musu.appContext.feed.post(truth_obj);
+      musu.appContext.feed.post(dare_obj);
+      
+      console.log("after appcontext.feed/before start_obj.post");
+      console.log("start_obj looks like this:"+ start_obj );
+      start_obj.post(truth_obj);
+      start_obj.post(dare_obj);
+      
+      console.log("after start_obj.post");
+      
+      console.log("querying subfeed...");
+      var arr = start_obj.query("type = 'truth'");
+      console.log("Got results: "+ JSON.stringify(arr));
+      musu.appContext.quit();  
+         **/   
+	});
+    
+    
+});
+$(function(){
+	$("#about").click(function(e) {
+		$(".start").css("display","none");
+		$(".about").css("display","inline");
+		$(".input").css("display","none");
+	});
+	$("#start").click(function(e) {
+		$(".start").css("display","none");
+		$(".about").css("display","none");
+		$(".input").css("display","inline");
+	});
+	$("#return").click(function(e) {
+		$(".start").css("display","inline");
+		$(".about").css("display","none");
+		$(".input").css("display","none");
+	});
+});
