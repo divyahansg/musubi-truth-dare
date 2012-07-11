e5577a42a8581cc90dda89bbfbc556999e6fab28
Nikhil <nikhil.narayen@gmail.com>
2012-07-11 14:13:55 -0700

    my version

--- musubi/apps/app_n.js
+++ musubi/apps/app_n.js
@@ -39,8 +39,10 @@ Musubi.ready(function(context) {
       start_obj_DbObj.post(truth_obj);
       start_obj_DbObj.post(dare_obj);
       
-      var temp_truth = start_obj_DbObj.query("type='truth'");
-      var temp_dare = start_obj_DbObj.query("type='dare'");
+      var temp_truth = new SocialKit.Obj(start_obj_DbObj.query("type='truth'"));
+      var temp_dare = new SocialKit.Obj(start_obj_DbObj.query("type='dare'"));
+      
+      
 	});
     
     
