a8f7fb9e2bb203c306c5b58139e0c6fcd21be22c
Divyahans Gupta <divyahansg@gmail.com>
2012-07-11 11:44:52 -0700

    to String feed

--- musubi/apps/app_n.js
+++ musubi/apps/app_n.js
@@ -43,7 +43,7 @@ Musubi.ready(function(context) {
       	
       	}, 200000);
       }
-    console.log(start_obj_DBObj);
+    console.log(musu.appContext.feed.toString());
 
       //while (!(start_obj_DbObj instanceof SocialKit.DbObj))
       //{
