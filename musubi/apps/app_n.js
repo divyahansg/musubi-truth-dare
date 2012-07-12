2c21e024075843010e1e7126aa530947a62ada24
Divyahans Gupta <divyahansg@gmail.com>
2012-07-12 11:27:56 -0700

    added user_obj; console++ and nested test

--- musubi/apps/app_n.js
+++ musubi/apps/app_n.js
@@ -33,11 +33,13 @@ Musubi.ready(function(context) {
       console.log("context.user[name] = " + context.user['name']);
       var user_json = {"type": "user", "id": userID, "name" : context.user['name']};
       user_obj = new SocialKit.Obj(user_json);
-      
+      console.log(JSON.stringify(user_obj));
       var data = musu.appContext.feed.query("type='truth_dare_state'", "_id desc limit 1")[0];
       start_obj_DbObj = new SocialKit.DbObj(data); 
       
       start_obj_DbObj.post(user_obj);
+      var test_user_obj = start_obj_DbObj.query("type='user'");
+      console.log("test_user_obj.length = " + test_user_obj.length);
       //musu.appContext.quit();
       
     });
