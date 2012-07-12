51fd581aee76c84329a27f60b75c034f8d125ee2
Divyahans Gupta <divyahansg@gmail.com>
2012-07-12 11:41:36 -0700

    added user_obj; no query consoled startgame

--- musubi/apps/app_n.js
+++ musubi/apps/app_n.js
@@ -36,6 +36,7 @@ Musubi.ready(function(context) {
       console.log((user_obj));
       var data = musu.appContext.feed.query("type='truth_dare_state'", "_id desc limit 1")[0];
       start_obj_DbObj = new SocialKit.DbObj(data); 
+      console.log("start_obj_DbObj = " + start_obj_DbObj);
       
       //start_obj_DbObj.post(user_obj);
       var test_user_obj = start_obj_DbObj.query("type='user'");
