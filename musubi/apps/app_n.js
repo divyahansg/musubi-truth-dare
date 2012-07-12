fe6c3ac3217d1ace0429f886603a2e2fc2f5475a
Divyahans Gupta <divyahansg@gmail.com>
2012-07-12 11:50:14 -0700

    delay

--- musubi/apps/app_n.js
+++ musubi/apps/app_n.js
@@ -33,15 +33,18 @@ Musubi.ready(function(context) {
       console.log("context.user[name] = " + context.user['name']);
       var user_json = {"id" : userID, "name" : context.user['name']};
       user_obj = new SocialKit.Obj({type: "user", json: user_json});
-      console.log((user_obj));
-      var data = musu.appContext.feed.query("type='truth_dare_state'", "_id desc limit 1");//[0]
-      console.log("data = " + JSON.stringify(data));
-      start_obj_DbObj = new SocialKit.DbObj(data); 
+      console.log((user_obj));      
+      setTimeout(func, 4000);
+		function func() {
+    		var data = musu.appContext.feed.query("type='truth_dare_state'", "_id desc limit 1");//[0]
+			console.log("data = " + JSON.stringify(data));
+		}
+      //start_obj_DbObj = new SocialKit.DbObj(data); 
       //console.log("start_obj_DbObj = " + start_obj_DbObj);
       
       //start_obj_DbObj.post(user_obj);
-      var test_user_obj = start_obj_DbObj.query("type='user'");
-      console.log("test_user_obj.length = " + test_user_obj.length);
+      //var test_user_obj = start_obj_DbObj.query("type='user'");
+      //console.log("test_user_obj.length = " + test_user_obj.length);
       //musu.appContext.quit();
       
     });
