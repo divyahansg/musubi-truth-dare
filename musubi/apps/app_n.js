f5d36924789adef01b0f0415ac0ad59d91dd1e12
Divyahans Gupta <divyahansg@gmail.com>
2012-07-11 19:26:14 -0700

    stored OBJs in array to access json.stringify with text

--- musubi/apps/app_n.js
+++ musubi/apps/app_n.js
@@ -78,7 +78,7 @@ Musubi.ready(function(context) {
 			var rand = Math.floor(Math.random()* (arr.length+1));
 			console.log("arr.length =" + arr.length);
 			console.log("arr[rand] = " + arr[rand]);
-			console.log("arr[rand].json = " + JSON.stringify(arr[rand].json));
+			console.log("arr[rand].json.text = " + JSON.stringify(arr[rand].json)['text']);
 			//$("#current_truth").append(rand_truth);
 		}
 	});
