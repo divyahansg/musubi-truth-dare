bec347ac1f4b3755636f31669ae127f4da3d15a9
Divyahans Gupta <divyahansg@gmail.com>
2012-07-11 22:06:08 -0700

    investigating error

--- musubi/apps/app_n.js
+++ musubi/apps/app_n.js
@@ -78,7 +78,7 @@ Musubi.ready(function(context) {
 			var rand = Math.floor(Math.random()* (arr.length+1));
 			console.log("arr.length = " + arr.length);
 			console.log("arr["+rand+"] = " + arr[rand]);
-			console.log("arr["rand+"].json.text = " + arr[rand].json['text']);
+			console.log("arr["+rand+"].json.text = " + arr[rand].json['text']);
 			$("#current_truth").append(arr[rand].json['text']);
 		}
 	});
