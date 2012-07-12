d25f27a435e56caea7433361b8b66921c2f1e27d
Divyahans Gupta <divyahansg@gmail.com>
2012-07-12 12:33:53 -0700

    probably errors + stringify

--- musubi/apps/app_n.js
+++ musubi/apps/app_n.js
@@ -106,8 +106,8 @@ Musubi.ready(function(context) {
     {
       var data = context.feed.query("type='truth_dare_state'", "_id desc limit 1")[0];
 	  var start_obj_DbObj = new SocialKit.DbObj(data); 
-	  var arr = new Array();
 	  var user_arr = start_obj_DbObj.query("type = 'user'");
+	  console.log("================================USER IN GETUSER IS = " + JSON.stringify(user_arr));
 	  for(i =0; i < user_arr.length; i++) {
 	  	temp_user = new SocialKit.Obj(user_arr[i]);
 	  	temp_ID = temp_user.id;
