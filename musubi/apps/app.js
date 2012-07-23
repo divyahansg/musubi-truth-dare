42adb78f1e14330fdb395f3ee42307325abd87dc
Divyahans Gupta <divyahansg@gmail.com>
2012-07-23 11:07:49 -0700

    file reader

--- musubi/apps/app.js
+++ musubi/apps/app.js
@@ -219,22 +219,26 @@ Musubi.ready(function(context) {
 	});
 	
 	$("#submit_dare").click(function(evt) {
+		console.log("=========SUBMIT CLICKED");
     	var files = evt.target.files; // FileList object
 		var f = files[0];
 	
   	    // Only process image files.
   	    if (!f.type.match('image.*')) {
-    		alert("Upload an image dumbass!");
+    		console.log("Upload an image dumbass!");
 	 	   return;
    	    }
 
    	    var reader = new FileReader();
+   	    console.log("==========CREATED FILEREADER");
 
    	    // Closure to capture the file information.
   	    reader.onload = (function(theFile) {
      	    return function(e) {
             // Render thumbnail.
+            console.log("================ENTERED META METHOD");
             $("#falcon").append("<img class=thumb src='" + e.target.result + "' title='" + escape(theFile.name) + "'/>");
+            console.log("==============FINISHED APPENDING");
           };
         })(f);
     }
