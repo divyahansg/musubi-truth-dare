47f0cc5cd0a8bb3f6543984cf4ea4af90a687107
Divyahans Gupta <divyahansg@gmail.com>
2012-07-23 15:04:48 -0700

    obj

--- musubi/apps/app.js
+++ musubi/apps/app.js
@@ -293,7 +293,7 @@ Musubi.ready(function(context) {
 			}
 		}
 		$(".img_viewer").css("display","inline");
-		$(".dashbaord".css("display","none");
+		$(".dashbaord").css("display","none");
 		
 	});
 	
