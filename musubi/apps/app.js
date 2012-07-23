dc4d5c91fad32f4aae39f7c9a82c820294bc376a
Divyahans Gupta <divyahansg@gmail.com>
2012-07-23 16:10:34 -0700

    obj

--- musubi/apps/app.js
+++ musubi/apps/app.js
@@ -282,36 +282,9 @@ Musubi.ready(function(context) {
 	});
 		
 	
-	$("#link").click(function(e) {
-		console.log("=======LINK CLICKED");
-		var name = $(this).attr('user_name');
-		var data = musu.appContext.feed.query("type='truth_dare_state'", "_id desc limit 1")[0]; //getting game state
-		var start_obj_DbObj = new SocialKit.DbObj(data); //create DbObj out of start
-		
-		var users = start_obj_DbObj.query("type='user'"); //getting all users
-		for (i=0; i<users.length; i++)
-		{
-			var temp_user_obj = new SocialKit.Obj(users[i]); //creating Obj of user
-			var temp_name = temp_user_obj.json['name']; //get name
-			if (name == temp_name) //if matched user
-			{
-				var temp_user_dbobj = new SocialKit.DbObj(users[i]); //make DbObj out of user
-				var temp_progress = temp_user_dbobj.query("type='progress'"); //querying for progress
-				var done_obj = new SocialKit.Obj(temp_progress[2]); //getting done obj
-				var img_src = done_obj.json['picture_src']; //getting img url
-				$("#img_container").append("<img height='800px' width='300px' src='" + img_src + "'/>"); //displaying image
-			}
-		}
-		$(".img_viewer").css("display","inline");
-		$(".dashboard").css("display","none");
-		
-	});
 	
 	function refreshDash()
 	{
-	
-	
-	
 		console.log("REFRESH DASH");
 		var truth_content = "";
 		var dare_content = "";
@@ -368,6 +341,33 @@ Musubi.ready(function(context) {
 			$(".done_div").css("display","inline");
 		}
 	}
+	
+	
+	$("#link").click(function(e) {
+		console.log("=======LINK CLICKED");
+		var name = $(this).attr('user_name');
+		var data = musu.appContext.feed.query("type='truth_dare_state'", "_id desc limit 1")[0]; //getting game state
+		var start_obj_DbObj = new SocialKit.DbObj(data); //create DbObj out of start
+		
+		var users = start_obj_DbObj.query("type='user'"); //getting all users
+		for (i=0; i<users.length; i++)
+		{
+			var temp_user_obj = new SocialKit.Obj(users[i]); //creating Obj of user
+			var temp_name = temp_user_obj.json['name']; //get name
+			if (name == temp_name) //if matched user
+			{
+				var temp_user_dbobj = new SocialKit.DbObj(users[i]); //make DbObj out of user
+				var temp_progress = temp_user_dbobj.query("type='progress'"); //querying for progress
+				var done_obj = new SocialKit.Obj(temp_progress[2]); //getting done obj
+				var img_src = done_obj.json['picture_src']; //getting img url
+				$("#img_container").append("<img src='" + img_src + "'/>"); //displaying image
+			}
+		}
+		$(".img_viewer").css("display","inline");
+		$(".dashboard").css("display","none");
+		
+	});
+	
 	
     function makeUser(context)
     {
