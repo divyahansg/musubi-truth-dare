ca4190d7722efd669a0f145b116b824dd506150a
Divyahans Gupta <divyahansg@gmail.com>
2012-07-25 11:06:54 -0700

    obj state

--- musubi/apps/app.js
+++ musubi/apps/app.js
@@ -56,7 +56,7 @@ Musubi.ready(function(context) {
       setTimeout(func, 1000);
 		function func() {
     		var data = musu.appContext.feed.query("type='truth_dare_state'", "_id desc limit 1")[0]; //getting game state
-		    var start_obj_DbObj = new SocialKit.DbObj(data); 
+		    start_obj_DbObj = new SocialKit.DbObj(data); 
 		    start_obj_DbObj.post(user_obj); //adding starting player to game
 		}
 		
